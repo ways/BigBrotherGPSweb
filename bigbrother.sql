da01b2dd82603926e45671cf73e5f1ab11f19058
Lars Falk-Petersen <dev@falk-petersen.no>
2014-09-17 21:37:43 +0200

    Updated sql files.

--- bigbrother.sql
+++ bigbrother.sql
@@ -3,18 +3,12 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Sep 17, 2014 at 01:34 PM
+-- Generation Time: Sep 17, 2014 at 09:36 PM
 -- Server version: 5.5.38
 -- PHP Version: 5.4.4-14+deb7u14
 
 SET SQL_MODE="NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
 
 --
 -- Database: `bigbrother`
@@ -45,7 +39,7 @@ CREATE TABLE IF NOT EXISTS `requests` (
   `deviceid` varchar(25) NOT NULL,
   `subscriberid` varchar(25) NOT NULL,
   PRIMARY KEY (`rid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=406 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=462 ;
 
 -- --------------------------------------------------------
 
@@ -57,10 +51,34 @@ CREATE TABLE IF NOT EXISTS `secrets` (
   `sid` bigint(20) unsigned NOT NULL AUTO_INCREMENT,
   `sname` varchar(25) NOT NULL,
   `type` varchar(25) NOT NULL,
+  `comment` varchar(50) NOT NULL,
   `avatar` blob NOT NULL,
   PRIMARY KEY (`sid`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=7 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
 
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `settings`
+--
+
+CREATE TABLE IF NOT EXISTS `settings` (
+  `key` varchar(25) NOT NULL,
+  `value` varchar(25) NOT NULL,
+  UNIQUE KEY `key` (`key`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
+
+CREATE TABLE IF NOT EXISTS `users` (
+  `uid` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `uname` varchar(25) NOT NULL,
+  `upassword` varchar(25) NOT NULL,
+  PRIMARY KEY (`uid`),
+  UNIQUE KEY `uid` (`uid`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2 ;
+
